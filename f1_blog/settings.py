29fdea24f14179ce50ece11f2b6ef20bb8b445cb
sbPetkov <svilen.b.petkov@gmail.com>
2024-03-20 13:00:32 +0200

    Initial commit for F1 blog

--- f1_blog/settings.py
+++ f1_blog/settings.py
@@ -104,12 +104,4 @@ MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % 'backendcloud'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AWS_ACCESS_KEY_ID = 'AKIATCKATZEC7OSAWVBJ'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'backendcloud'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'eu-north-1'
-AWS_S3_FILE_OVERWRITE = True
-AWS_DEFAULT_ACL = None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
